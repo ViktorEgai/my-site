3c2bc7e596ee9f838579128180fb3b0c0651ff7a
ViktorEgai <egai.viktor94@gmail.com>
2021-06-18 11:51:02 +0300

    верстка

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+let gulp = require('gulp'),
+     sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+  return gulp.src('app/sass/**/*.sass') 
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function() {
+  return gulp.src('app/*.html') 
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server:{
+      baseDir: "app/"
+    }
+  });
+});
+gulp.task('script', function() {
+  return gulp.src('app/js/*.js') 
+  .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('js',function(){
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/swiper/swiper-bundle.min.js'
+  ])
+  .pipe(concat('libs.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/js'))
+  .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('watch', function(){
+  gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'))
+  gulp.watch('app/*.html', gulp.parallel('html'))
+  gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+gulp.task('default', gulp.parallel('sass','js','browser-sync', 'watch'));
